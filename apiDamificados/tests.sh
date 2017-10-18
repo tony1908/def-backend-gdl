b856f3f6a17e07d9c17877bff441575122940db5
Antonio <toony1908@gmail.com>
2017-10-17 21:26:43 -0500

    circle

--- apiDamificados/tests.sh
+++ apiDamificados/tests.sh
@@ -3,4 +3,4 @@ python manage.py migrate lugares zero
 python manage.py migrate User zero
 python manage.py makemigrations
 python manage.py migrate
-gunicorn --bind 0.0.0.0:8000 apiDamificados.wsgi:application 
+python manage.py test
