2d29c501570cf068f527918e93f8f094acd62fc3
Antonio <toony1908@gmail.com>
2017-10-18 15:56:30 -0500

    deploy

--- setup-heroku.sh
+++ setup-heroku.sh
@@ -3,6 +3,7 @@
   sudo mkdir -p /usr/local/lib /usr/local/bin
   sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
   sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
+  sudo heroku plugins:install heroku-container-tools
      
   sudo cat > ~/.netrc << EOF
   machine api.heroku.com
