24e5270f189cc0123badf594f2e84390a953ab07
Antonio <toony1908@gmail.com>
2017-10-18 21:04:39 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,4 @@
-# if [ "${CIRCLE_BRANCH}" == "master" ]; then
-# 	echo "Mandndo a heroku"
-# 	heroku container:push web --app $HEROKU_APP_NAME
-# 	echo "Se mando a heroku"
-# 	# git push --force git@heroku.com:$HEROKU_APP_NAME.git HEAD:refs/heads/master
-# 	# heroku run python manage.py deploy
-# 	# heroku restart
-# fi
-
-echo "Mandndo a heroku"
+echo "Mandando a heroku"
 sudo heroku container:login
 sudo heroku container:push web --app $HEROKU_APP_NAME
 echo "Se mando a heroku"
